0680086d33d9813dd1ae780f0c723093180e1950
ameer <ameermohammadramzani@gmail.com>
2021-02-25 16:19:49 +0330

    first commit

---
+++ flaska/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaska.sqlite'),
+    )
+    
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hellp')
+    def hello():
+        return '<h1>Hello world.</h1>'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+    return app
